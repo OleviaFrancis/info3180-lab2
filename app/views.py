87c15811e5bcfce5a804317b6cac1fe6cbd3c0d0
Olevia Francis <quetysha@gmail.com>
2019-02-08 14:42:05 -0500

    Initial commit

--- app/views.py
+++ app/views.py
@@ -7,12 +7,16 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from datetime import datetime
 
 ###
 # Routing for your application.
 ###
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', startdate=format_date_joined(14, 2, 2018))
+    
 @app.route('/')
 def home():
     """Render website's home page."""
@@ -56,3 +60,7 @@ def page_not_found(error):
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="8080")
+
+def format_date_joined(day, month, years):
+    a = datetime(years,month,day)
+    return a.strftime("%B, %Y") 
